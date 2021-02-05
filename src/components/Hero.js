cac291dad62793554a8891b5d7c8c421509d6031
Kulahin Dmitry <dkulahin@gmail.com>
2021-02-05 11:44:25 +0300

    Fix Get Involved marquee bug

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -98,10 +98,12 @@ export default function Hero() {
 
   useEffect(() => {
     setTimeout(()=>{
-      const captionLength = +marqueeEl.current.offsetWidth;
-      const period = (captionLength/80);
-      marqueeEl.current.style.textShadow = `${captionLength}px 0 currentColor, calc(${captionLength}px * 2) 0 currentColor`;
-      marqueeEl.current.style.animationDuration = `${period}s`;
+      if (marqueeEl.current !== null){
+        const captionLength = +marqueeEl.current.offsetWidth;
+        const period = (captionLength/80);
+        marqueeEl.current.style.textShadow = `${captionLength}px 0 currentColor, calc(${captionLength}px * 2) 0 currentColor`;
+        marqueeEl.current.style.animationDuration = `${period}s`;
+      }
     }, 1000);
     
   });
