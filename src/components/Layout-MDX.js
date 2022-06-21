e263061e7931ce016ca9e17e4077de667a83a744
Will Howes <willmhowes@gmail.com>
2022-06-21 11:20:12 -0700

    attempt redo of principles in p1.mdx

--- src/components/Layout-MDX.js
+++ src/components/Layout-MDX.js
@@ -1,37 +1,57 @@
 import React, { useState, useEffect } from 'react'
+import { MDXProvider } from '@mdx-js/react'
 import favicon from '../img/favicon.svg'
 import Helmet from 'react-helmet'
+import {FaqQ, FaqA} from './LayoutPrinciples'
 
 import Navbar from './Navbar'
 
 export default function PageLayout(props) {
+  const [menuOpen, setMenuOpen] = useState(false)
 
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  let content = props.children;
+  let content = props.children
   console.log(props)
 
   const menuLinkClick = () => {
-    setMenuOpen(()=>{
-      return !menuOpen;
+    setMenuOpen(() => {
+      return !menuOpen
     })
   }
 
   const closeMenu = () => {
-    setMenuOpen(false);
+    setMenuOpen(false)
   }
 
-  let contentInlineStyle = menuOpen ? {display: "none"} : {};
+  let contentInlineStyle = menuOpen ? { display: 'none' } : {}
 
   return (
     <div>
       <Helmet title="DWeb">
-          <link rel="icon" href={favicon} />
+        <link rel="icon" href={favicon} />
       </Helmet>
-      <Navbar menuLinkClick={menuLinkClick} menuOpen={menuOpen} closeMenu={closeMenu} />
+      <Navbar
+        menuLinkClick={menuLinkClick}
+        menuOpen={menuOpen}
+        closeMenu={closeMenu}
+      />
       <div className="content" style={contentInlineStyle}>
-        {content}
+        <div className="container faq">
+          <div className="row">
+            <div className="col col-12 col-xs-12">
+              <div className="faq-sections faq-pair open">
+                <MDXProvider
+                  components={{
+                    h1: FaqQ,
+                    ol: FaqA,
+                  }}
+                >
+                  {content}
+                </MDXProvider>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  );
+  )
 }
