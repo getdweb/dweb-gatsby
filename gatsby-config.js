0b04fb1bbda7f0257bb9e39521fa239e4a2215ea
Will Howes <willmhowes@gmail.com>
2022-06-28 16:17:00 -0700

    comment out wordpress dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,42 +13,57 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        defaultLayouts: { default: path.resolve('./src/components/Layout-MDX.js') },
-      },
-    },
-    {
-      resolve: 'gatsby-source-wordpress',
-      options: {
-        // The base url to your WP site.
-        baseUrl: process.env.BASE_URL || 'dweb',
-        // WP.com sites set to true, WP.org set to false
-        hostingWPCOM: false,
-        // The protocol. This can be http or https.
-        protocol: process.env.PROTOCOL || 'http',
-        // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: true,
-        auth: {},
-        // Set to true to debug endpoints on 'gatsby build'
-        verboseOutput: false,
-        excludedRoutes: [
-          "**/wp-site-health/**",
-          "**/yoast/**",
-          "**/wp/v2/users/**",
-          "**/batch/**",
-          "**/wp-rest-yoast-meta/**",
-          "**/wp/v2/block-types**",
-          "**/wp/v2/settings**",
-          "**/wp/v2/themes**",
-          "**/wp/v2/plugins**",
-          "**/wp/v2/block-directory/**",
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-mdx`,
+    //   options: {
+    //     defaultLayouts: { default: path.resolve('./src/components/Layout-MDX.js') },
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-source-wordpress',
+    //   options: {
+    //     // The base url to your WP site.
+    //     baseUrl: process.env.BASE_URL || 'dweb',
+    //     // WP.com sites set to true, WP.org set to false
+    //     hostingWPCOM: false,
+    //     // The protocol. This can be http or https.
+    //     protocol: process.env.PROTOCOL || 'http',
+    //     // Use 'Advanced Custom Fields' Wordpress plugin
+    //     useACF: true,
+    //     auth: {},
+    //     // Set to true to debug endpoints on 'gatsby build'
+    //     verboseOutput: false,
+    //     excludedRoutes: [
+    //       "**/wp-site-health/**",
+    //       "**/yoast/**",
+    //       "**/wp/v2/users/**",
+    //       "**/batch/**",
+    //       "**/wp-rest-yoast-meta/**",
+    //       "**/wp/v2/block-types**",
+    //       "**/wp/v2/settings**",
+    //       "**/wp/v2/themes**",
+    //       "**/wp/v2/plugins**",
+    //       "**/wp/v2/block-directory/**",
+    //     ],
+    //   },
+    // },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    // `gatsby-transformer-yaml`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     // name: `data`,
+    //     path: `${__dirname}/src/data/`,
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `markdownx`,
+    //     path: `${__dirname}/src/components/`,
+    //   },
+    // },
     // {
     //   // Removes unused css rules
     //   resolve:'gatsby-plugin-purgecss',
