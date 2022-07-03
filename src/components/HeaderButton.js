6224cf6dfb2af61575717c8bff899850bf7d2573
Will Howes <willmhowes@gmail.com>
2022-07-03 15:18:07 -0700

    remove data call in HeaderButton.js

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -1,24 +1,10 @@
 import React from 'react'
-import { useStaticQuery, graphql, Link } from 'gatsby'
+import { Link } from 'gatsby'
 
 export default function HeaderButton() {
-  const data = JSON.parse(`{
-  "data": {
-    "allWordpressAcfOptions": {
-      "nodes": [
-        {
-          "options": {
-            "header_button_url": "/get-involved"
-          }
-        }
-      ]
-    }
-  }
-}`)
-
-  const {header_button_url} = data.data.allWordpressAcfOptions.nodes[0].options;
-
   return (
-    <Link to={header_button_url} className="navbar__button">Get involved</Link>
+    <Link to="/get-involved" className="navbar__button">
+      Get involved
+    </Link>
   )
 }
