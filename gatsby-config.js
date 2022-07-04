111dcae983ef9236abb0c968181fce0ef77a5822
Will Howes <willmhowes@gmail.com>
2022-07-03 17:13:47 -0700

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,14 +22,15 @@ module.exports = {
     // {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    // `gatsby-transformer-yaml`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     // name: `data`,
-    //     path: `${__dirname}/src/data/`,
-    //   },
-    // },
+    // `gatsby-transformer-json`,
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
