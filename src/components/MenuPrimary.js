c4718f6383e021a9dd8510b9c6f9d21f801550c7
Will Howes <willmhowes@gmail.com>
2022-07-11 13:00:20 -0700

    Update menus to utilitize the data layer

--- src/components/MenuPrimary.js
+++ src/components/MenuPrimary.js
@@ -2,60 +2,31 @@ import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 
 export default function MenuPrimary(props) {
-  const data = JSON.parse(`{
-    "data": {
-      "allWordpressMenusMenusItems": {
-        "edges": [
-          {
-            "node": {
-              "id": "344797a4-3d87-5fbc-87d2-a32a5bb9765d",
-              "slug": "primary",
-              "name": "primary",
-              "items": [
-                {
-                  "title": "Mission",
-                  "url": "/#mission",
-                  "object_id": "18"
-                },
-                {
-                  "title": "Nodes",
-                  "url": "/#nodes",
-                  "object_id": "20"
-                },
-                {
-                  "title": "Events",
-                  "url": "/#events",
-                  "object_id": "19"
-                },
-                {
-                  "title": "Voices",
-                  "url": "/#voices",
-                  "object_id": "21"
-                },
-                {
-                  "title": "Sponsors",
-                  "url": "/#partners",
-                  "object_id": "22"
-                }
-              ]
-            }
+  const data = useStaticQuery(
+    graphql`
+      query {
+        menuYaml(label: { eq: "primary" }) {
+          items {
+            object_id
+            title
+            url
           }
-        ]
+        }
       }
-    }
-  }`)
+    `
+  )
 
-  const menu_items = data.data.allWordpressMenusMenusItems.edges[0].node.items
+  const menu_items = data.menuYaml.items
 
   return (
     <div className="navbar__menus__item menu-primary" id="menu-primary">
-      {Object.entries(menu_items).map(([key,item]) => (
+      {Object.entries(menu_items).map(([key, item]) => (
         <Link
           className="menu-primary__item"
           to={item.url}
           key={item.object_id}
           onClick={props.closeMenu}
-          >
+        >
           {item.title}
         </Link>
       ))}
