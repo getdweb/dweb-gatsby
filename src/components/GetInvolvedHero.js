e09de6f6aaf46ce239335e5bae5c87474b334bb8
Will Howes <willmhowes@gmail.com>
2022-07-11 20:21:11 -0700

    make GetInvolvedHero static

--- src/components/GetInvolvedHero.js
+++ src/components/GetInvolvedHero.js
@@ -1,40 +1,22 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
 
 export default function GetInvolvedHero() {
-
-  const data = JSON.parse(`{
-  "data": {
-    "allWordpressAcfOptions": {
-      "nodes": [
-        {
-          "options": {
-            "get_involved_hero_header": "A web for all requires everyone to get involved",
-            "get_involved_hero_image": {
-              "localFile": {
-                "url": "https://getdweb.net/wp-content/uploads/2020/11/Get-involved-hero.png"
-              }
-            },
-            "get_involved_hero_image_mobile": {
-              "localFile": {
-                "url": "https://getdweb.net/wp-content/uploads/2021/01/Get-involved-hero-mobile.jpg"
-              }
-            }
-          }
-        }
-      ]
-    }
-  }
-}`)
-
-  const options = data.data.allWordpressAcfOptions.nodes[0].options;
-
   return (
     <div className="get-involved-hero">
-      <div className="get-involved-hero__image d-none d-md-block" style={{backgroundImage: `url(${options.get_involved_hero_image.localFile.url})`}}></div>
-      <div className="get-involved-hero__image d-md-none" style={{backgroundImage: `url(${options.get_involved_hero_image_mobile.localFile.url})`}}></div>
+      <div
+        className="get-involved-hero__image d-none d-md-block"
+        style={{
+          backgroundImage: `url(https://getdweb.net/wp-content/uploads/2020/11/Get-involved-hero.png)`,
+        }}
+      ></div>
+      <div
+        className="get-involved-hero__image d-md-none"
+        style={{
+          backgroundImage: `url(https://getdweb.net/wp-content/uploads/2021/01/Get-involved-hero-mobile.jpg)`,
+        }}
+      ></div>
       <div className="get-involved-hero__header">
-        {options.get_involved_hero_header}
+        A web for all requires everyone to get involved
       </div>
     </div>
   )
