16790f76926a6ca048fbf4f94b368536a59e680b
Will Howes <willmhowes@gmail.com>
2022-07-13 17:58:08 -0700

    simplify node packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,6 @@ module.exports = {
     // {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    // `gatsby-transformer-json`,
     `gatsby-transformer-yaml`,
     {
       resolve: `gatsby-source-filesystem`,
