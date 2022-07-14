e36773a1f19704d201226b56019b23c3cd2b3b5a
Will Howes <willmhowes@gmail.com>
2022-07-13 18:20:53 -0700

    configure purgecss plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,20 @@
 const path = require('path')
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`
-});
-console.log(`Using .env.${process.env.NODE_ENV}`);
 module.exports = {
   siteMetadata: {
     title: 'Get Dweb Dot Net',
   },
-  // pathPrefix: `/aaa/bbb`,
-  // assetPrefix: `/ccc/ddd`,
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        implementation: require("node-sass"),
-      },
-    },
-    // {
-    //   resolve: `gatsby-plugin-mdx`,
-    //   options: {
-    //     defaultLayouts: { default: path.resolve('./src/components/Layout-MDX.js') },
-    //   },
-    // },
-    // {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require('node-sass'),
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -36,22 +23,26 @@ module.exports = {
       },
     },
     // {
-    //   resolve: `gatsby-source-filesystem`,
+    //   resolve: `gatsby-plugin-mdx`,
     //   options: {
-    //     name: `markdownx`,
-    //     path: `${__dirname}/src/components/`,
+    //     defaultLayouts: {
+    //       default: path.resolve('./src/components/Layout-MDX.js'),
+    //     },
     //   },
     // },
-    // {
-    //   // Removes unused css rules
-    //   resolve:'gatsby-plugin-purgecss',
-    //   options: {
-    //     // Activates purging in gatsby develop
-    //     develop: true,
-    //     // Purge only the main css file
-    //     purgeOnly: ['/all.sass'],
-    //   },
-    // }, // must be after other CSS plugins
-    // 'gatsby-plugin-netlify', // make sure to keep it last in the array
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        develop: true,
+        printRejected: true,
+        content: [
+          path.join(process.cwd(), 'src/**/!(*.d).{ts,js,jsx,tsx}'),
+          path.join(
+            process.cwd(),
+            'node_modules/swiper/react/!(*.d).{ts,js,jsx,tsx}'
+          ),
+        ],
+      },
+    }, // must be after other CSS plugins
   ],
 }
