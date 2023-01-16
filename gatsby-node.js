b4db0208eb6a40d60c71e5bb40bff30891e06c5b
Will Howes <willmhowes@gmail.com>
2023-01-16 14:17:12 -0800

    make image sourcing relative

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,11 @@
 const _ = require('lodash')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const pMap = require('p-map');
+const globby = require('globby');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 const { createFilePath } = require('gatsby-source-filesystem')
 
 const getOnlyPublished = (edges) =>
@@ -2832,3 +2838,25 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+TRANSFORM_CONCURRENCY = 10
+
+exports.onPostBuild = async () => {
+  // Replaces all image urls with the correct relative paths
+  const paths = await globby(['public/**/*.html']);
+
+  await pMap(paths, async (path) => {
+    const buffer = await readFileAsync(path);
+    let contents = buffer.toString();
+
+    // Skip if there's nothing to do
+    if (!contents.includes('/images')) {
+      return;
+    }
+
+    contents = contents
+      .replace(/\/images\//g, () => '../images/');
+
+    await writeFileAsync(path, contents);
+  }, { concurrency: TRANSFORM_CONCURRENCY });
+};
