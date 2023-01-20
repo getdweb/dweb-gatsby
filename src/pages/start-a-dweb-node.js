65f61d82c18499b072b2937218db049a8688efc2
Will Howes <willmhowes@gmail.com>
2023-01-20 10:47:43 -0800

    create start-a-dweb-node page

---
+++ src/pages/start-a-dweb-node.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Layout from '../components/Layout'
+import PageBlockOpening from '../components/PageBlockOpening'
+import PageBlockFeature from '../components/PageBlockFeature'
+import data_array from '../../static/page-data/start-a-dweb-node.yaml'
+
+function StartADWebNodePage() {
+    const content = (
+        <div>
+            <div className="master-page-content">
+                <PageBlockOpening fields={data_array[0].PageBlockOpening} />
+                <PageBlockFeature fields={data_array[1].PageBlockFeature} />
+            </div>
+            <Footer />
+        </div>
+    )
+
+    return <Layout content={content} />
+}
+
+export default StartADWebNodePage
