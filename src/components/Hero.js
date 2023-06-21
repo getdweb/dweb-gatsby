6954d72029e3b5699c7caaaee7da0335370292da
Arkadiy Kukarkin <arkadiy@archive.org>
2023-06-21 11:39:00 -0700

    remove CTA for camp

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -118,7 +118,7 @@ export default function Hero() {
           href="https://dwebcamp.org/"
           dangerouslySetInnerHTML={{
             __html:
-              'DWeb Camp is back in 2023!<br>\r\nJune 21-25, Navarro, CA. Join us!',
+              'DWeb Camp is back in 2023!<br>\r\nJune 21-25, Navarro, CA.',
           }}
           rel="noreferrer"
         />
