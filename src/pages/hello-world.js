a35882016a460beddacccb23312190f29248e467
Kevin Nguyen <lightandluck@protonmail.com>
2025-06-05 19:53:32 -0700

    Add hello-world page

---
+++ src/pages/hello-world.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Footer from '../components/Footer'
+
+function SocialMediaCommsJobDescriptionPage() {
+  const content = (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-xs-12">
+            <div className="job-description">
+              <h1 style={{ fontSize: '46px' }}>
+                Hello World!
+                <h2 className="highlight">Links to welcome you to the DWeb</h2>
+              </h1>
+              <hr />
+              <div className="building-block__text">
+                <ul>
+                  <li>
+                    <a href="/dweb-webinar-series" target="blank">
+                      What is the DWeb? - DWeb webinar series
+                    </a>
+                  </li>
+                  <li>
+                    <a href="//dweb.events" target="blank">
+                      DWeb Events - Calendar of events from around the world
+                    </a>
+                    <ul>
+                      <li>
+                        <a
+                          href="https://airtable.com/appTI5ffWtVBghxjJ/pagtSqKejqCT4bI2T/form"
+                          target="blank"
+                        >
+                          Propose an event
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a
+                      href="https://mailchi.mp/archive/dweb-updates"
+                      target="blank"
+                    >
+                      Subscribe to our newsletter
+                    </a>
+                  </li>
+                </ul>
+                {/* End content div */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+
+  return <Layout content={content} />
+}
+
+export default SocialMediaCommsJobDescriptionPage
