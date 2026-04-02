5731073ef124406a101769097cc4c093515a6d1c
Kevin Nguyen <lightandluck@protonmail.com>
2026-04-02 01:08:52 -0700

    Add media-kit-page

---
+++ src/pages/media-kit-2026.js
@@ -0,0 +1,283 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Footer from '../components/Footer'
+
+function MediaKit2026Page() {
+  const content = (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-xs-12">
+            <div className="custom-content building-block__text">
+              <h1>
+                DWeb Camp 2026: <br />
+                Media Kit
+              </h1>
+              <p>
+                Dear DWeb Camp supporter, here is key information to help
+                promote DWeb Camp 2026. For questions or collaboration ideas,
+                contact{' '}
+                <a href="mailto:dwebcamp@archive.org">dwebcamp@archive.org</a>.
+              </p>
+              <p>
+                Access all media resources and boilerplate materials in the{' '}
+                <a href="https://office.dod.ngo/drive/#/2/drive/view/8TXa+iRz9u7OmhlAdrNU9mM-wpijo9fJV23Si0H79tc/">
+                  info kit folder
+                </a>
+                .
+              </p>
+              <a
+                href="https://office.dod.ngo/drive/#/2/drive/view/8TXa+iRz9u7OmhlAdrNU9mM-wpijo9fJV23Si0H79tc/"
+                className="btn building-block__btn"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Download the media kit
+              </a>
+              <figure>
+                <img
+                  src="/images/media-kit/media-kit-hero-image.jpg"
+                  alt="DWeb Camp 2026 media kit hero"
+                />
+              </figure>
+              <h2>About DWeb Camp</h2>
+              <p>
+                DWeb Camp 2026: <strong>Root Systems</strong> is a five-day
+                gathering in nature for builders and dreamers dedicated to
+                building a decentralized web: private, resilient, and beyond the
+                control of a few central powers.
+              </p>
+              <p>
+                This year rotates around one key understanding:{' '}
+                <strong>What Connects Us is Mostly Underground.</strong> Real
+                power does not always appear at the surface. It spreads through
+                networks of interconnected nodes that collaborate, share
+                resources, and endure even when individual nodes disappear.
+              </p>
+              <p>
+                DWeb Camp is dedicated to tools for real-world problems,
+                including censorship resistance, identity for the stateless, and
+                sovereignty over data. Since 2016,{' '}
+                <a href="https://getdweb.net/">DWeb</a>, an independent project
+                founded by the nonprofit{' '}
+                <a href="https://archive.org/">Internet Archive</a>, has
+                gathered builders, researchers, activists, artists, educators,
+                and families to learn and build together around shared{' '}
+                <a href="https://getdweb.net/principles/">principles</a> of
+                trust, human agency, mutual respect, and ecological awareness.
+              </p>
+              <p>
+                DWeb Camp: Root Systems invites participants to build
+                technologies that, like living ecosystems, adapt, regenerate,
+                and endure.
+              </p>
+
+              <h2>DWeb Camp 2026 Programmatic Tracks</h2>
+              <p>
+                This year includes ten tracks to shape conversations and create
+                an engaging environment where ideas flourish:
+              </p>
+              <ul>
+                <li>Anti-Authoritarian Stack</li>
+                <li>Cultivating Tech for Food Sovereignty</li>
+                <li>Decentralized Design</li>
+                <li>Decentralized Hardware / Local Community Networks</li>
+                <li>Open Social Web</li>
+                <li>Peer-to-Peer &amp; Local First</li>
+                <li>Public AI</li>
+                <li>Solidarity Tech</li>
+                <li>Sustaining Infrastructure</li>
+                <li>Intergenerational Growth</li>
+              </ul>
+              <ul>
+                <li>
+                  <em>Seedlings</em>, Children&apos;s Track - under 12 years old
+                </li>
+                <li>
+                  <em>Saplings</em>, Teen Track - 13-17 years old
+                </li>
+                <li>
+                  <em>Old Growth</em> - Pioneers of the Decentralized Web
+                </li>
+              </ul>
+              <p>
+                Curators and subject-area specialists help invite diverse
+                speakers and design tracks. You can self-select a track for your
+                proposal, but fitting into a track is not required for a session
+                to be accepted.
+              </p>
+              <p>
+                Read more about this year&apos;s tracks:{' '}
+                <a href="https://dwebcamp.org/proposals/">
+                  https://dwebcamp.org/proposals/
+                </a>
+              </p>
+
+              <h2>Social Media and Cross-Promotion</h2>
+              <p>
+                We are grateful if you share your participation in DWeb Camp
+                across your channels and tag our profiles:
+              </p>
+              <ul>
+                <li>
+                  Fediverse:{' '}
+                  <a href="https://social.coop/dweb">@dweb@social.coop</a>
+                </li>
+                <li>
+                  Bluesky:{' '}
+                  <a href="https://bsky.app/profile/getdweb.net">
+                    @getdweb.net
+                  </a>
+                </li>
+                <li>
+                  LinkedIn:{' '}
+                  <a href="https://www.linkedin.com/company/101720699/">
+                    DWeb Community
+                  </a>
+                </li>
+                <li>
+                  X: <a href="https://x.com/GETDWeb">@getdweb</a>
+                </li>
+                <li>
+                  Mailing list:{' '}
+                  <a href="https://getdweb.net/newsletter/">
+                    https://getdweb.net/newsletter/
+                  </a>
+                </li>
+              </ul>
+
+              <p>Key partner profiles:</p>
+              <ul>
+                <li>
+                  <a href="https://archive.org/">Internet Archive</a>:&nbsp;
+                  <a href="https://mastodon.archive.org/@internetarchive">
+                    Fediverse
+                  </a>
+                  ,{' '}
+                  <a href="https://bsky.app/profile/internetarchive.org">
+                    Bluesky
+                  </a>
+                  ,{' '}
+                  <a href="https://www.linkedin.com/company/internet-archive">
+                    LinkedIn
+                  </a>
+                  , <a href="https://x.com/internetarchive/">X</a>,{' '}
+                  <a href="https://www.instagram.com/internetarchive/">
+                    Instagram
+                  </a>
+                </li>
+                <li>
+                  <a href="https://internetarchive.eu/">
+                    Internet Archive Europe
+                  </a>
+                  :{' '}
+                  <a href="https://toot.community/@internetarchiveeurope">
+                    Fediverse
+                  </a>
+                  ,{' '}
+                  <a href="https://bsky.app/profile/internetarchive.eu">
+                    Bluesky
+                  </a>
+                  ,{' '}
+                  <a href="https://www.linkedin.com/company/internet-archive-europe">
+                    LinkedIn
+                  </a>
+                </li>
+                <li>
+                  <a href="https://dod.ngo/">Department of Decentralization</a>:
+                  <a href="https://bsky.app/profile/dod.ngo"> Bluesky</a>,{' '}
+                  <a href="https://x.com/dod_berlin">X</a>
+                </li>
+              </ul>
+
+              <h2>Sample Social Posts</h2>
+              <blockquote>
+                I&apos;m participating in #DWebCamp 2026: Root Systems!
+                <br />
+                <br />
+                From July 8-12, join me and hundreds of builders and dreamers
+                from all over the globe to discover, tinker, make, and ideate
+                the decentralized technologies we truly need.
+                <br />
+                <br />
+                @getdweb https://dwebcamp.org
+              </blockquote>
+              <blockquote>
+                DWeb Camp 2026: Root Systems is a five-day gathering to
+                discover, envision, and build the decentralized technologies of
+                today and tomorrow, preserving the values that guided the
+                origins of the Web.
+                <br />
+                <br />
+                Join me and hundreds of campers on July 8-12 in Alte Holle,
+                Germany!
+                <br />
+                <br />
+                @getdweb https://dwebcamp.org
+              </blockquote>
+
+              <p>
+                You can also share this DWeb Camp video:{' '}
+                <a href="https://youtu.be/zx7Dduv0Hd4">
+                  https://youtu.be/zx7Dduv0Hd4
+                </a>
+              </p>
+              <p>
+                More information about DWeb as a movement and community: <br />
+                <a href="https://getdweb.net/">https://getdweb.net</a>
+              </p>
+
+              <h2>Images from Past Camps</h2>
+              <p>
+                View a selection of images from past camps in the{' '}
+                <a href="https://office.dod.ngo/drive/#/2/drive/view/8TXa+iRz9u7OmhlAdrNU9mM-wpijo9fJV23Si0H79tc/">
+                  info kit folder
+                </a>{' '}
+                and explore the full archive on the{' '}
+                <a href="https://archive.org/search?query=dweb+camp&sort=-downloads">
+                  Internet Archive
+                </a>
+                .
+              </p>
+
+              <h2>Relevant Links</h2>
+              <ul>
+                <li>
+                  <a href="https://getdweb.net/berlin-trip/">
+                    How DWeb Camp is Being Built in Berlin
+                  </a>
+                </li>
+                <li>
+                  <a href="https://getdweb.net/alte-hoelle/">
+                    Introducing the DWeb Camp 2026 Venue: Alte Holle
+                  </a>
+                </li>
+                <li>
+                  <a href="https://dwebcamp.org/proposals/">
+                    DWeb Camp 2026 Tracks
+                  </a>
+                </li>
+                <li>
+                  <a href="https://dwebcamp.org/pollinators/">
+                    DWeb Camp Pollinators Program
+                  </a>
+                </li>
+              </ul>
+              <figure>
+                <img
+                  src="/images/media-kit/media-kit-footer-image.jpg"
+                  alt="DWeb Camp 2026 media kit hero"
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+
+  return <Layout content={content} />
+}
+
+export default MediaKit2026Page
